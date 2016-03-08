9f2d932bb75f2d32cafee717710416a115af7776
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-03-07 19:19:27 -0800

    Updated files

--- __15_Command_line_arguments.py
+++ __15_Command_line_arguments.py
@@ -1,16 +1,54 @@
 #! /library/Frameworks/Python.framework/Versions/3.5/python3.5
 
-import getopt, sys, argparse
+import sys, argparse
 
-# python getopt module helps to parse command line arguments
 # python sys mode provides access to all command line arguments passed to program (sys.argv)
+#  - sys.argv is the list of command arguments
+#  - len(sys.argv) is the number of command line arguments
+#  - sys.arg[0] is the name of the file, then the following numbers are arguments
 
-# sys.argv is the list of command arguments
-# len(sys.argv) is the number of command line arguments
-# sys.arg[0] is the name of the file, then the following numbers are arguments
+# Example:
+# print('Here are the arguments passed to the script')
+# print(sys.argv)
+# print('Here is the name of the script being executed')
+# print(sys.argv[0])
 
-print('Here are the arguments passed to the script')
-print(sys.argv)
-print('Here is the name of the script being executed')
-print(sys.argv[0])
+# ARGPARSE - module to parse command line arguments
+# By default, argparse treats options as strings, unless we define the type
 
+# 1. create a parser
+parser = argparse.ArgumentParser(description='Introduction to argparse')
+
+# 1.1 Optional: create mutually exclusive options
+group = parser.add_mutually_exclusive_group()
+
+# 2. add arguments to the program
+#  - There are optional arguments (defined like -v), and positional/required (defined as v)
+#  - Short version is -v, long version is --version
+#  - action=store_true defines boolean type, if present it is true, if not it is fault
+
+parser.add_argument("-i", help='specify the input filename', required=True)
+parser.add_argument("-o", "--output", help="specify output file")
+parser.add_argument("square", help='number of iterations', type=int)
+parser.add_argument("filename", help='filename to create during execution')
+parser.add_argument("-v", "--verbose", help='define the verbosity level', action="store_true")
+parser.add_argument("--time", help="times to run", type=int, choices=[1,2,3,4,5])
+group.add_argument("--quiet", help='quiet execution mode', action='store_true')
+group.add_argument("--loud", help='loud execution mode', action='store_true')
+
+
+# 3. Parse all arguments into some class. Typically that would be args.
+args = parser.parse_args()
+
+# 4. Work with variables. They are accesssible through class.
+print('\n')
+print('These are the parameters'.center(60, '='))
+print(args.square, " - this is the square the was passed to the program")
+print(args.filename, " - this file will be created during execution")
+print(args.i, ' - here is the input filename')
+print(args.time, ' - number of times to run')
+if args.quiet:
+    print('Mode is quiet')
+if args.loud:
+    print('Mode is loud')
+print('\n')
