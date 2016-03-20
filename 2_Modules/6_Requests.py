babe19929c810a5d4e980515dfa8a69bfdba6678
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-03-20 16:44:53 -0700

    Test

--- 2_Modules/6_Requests.py
+++ 2_Modules/6_Requests.py
@@ -1,12 +1,21 @@
 #! /library/Frameworks/Python.framework/Versions/3.5/python3.5
 # ===================================================================================================
-# +++++++++ Requests module +++++++++
-# requests.get('url') - funciton takes a string of a URL to download
+# __________________ Requests module __________________
+# requests.get('url') - function takes a string of a URL to download
 #                     - you assign the response.get() to a variable
-# requests.object.text - the html response is stored as a string (web page)
-# requests.object.status_code - response status code
+# requests.get('url', auth={'username', 'pass'}) - authentication
+# requests.post('url', data={'key': 'value') - make a post method request
+# requests.get('url', params={'x': 'y'} - pass parameters as part of URL
+# requests.get('url', headers={'header': 'value'} - create the list of headers
+# __________________ Object parameters (object is 'r') __________________
+# r.url - return used URL
+# r.text - the html response is stored as a string (web page)
+# r.json() - the json response stored as dictionary
+# r.status_code - response status code (404, 200, etc.)
+# r.headers['content-type'] - retrieve certain headers
+# r.encoding - retrieve encoding
 # raise_for_status(requests.objects) - this method will raise an exception if there was an error downloading the page
-# +++++++++ Copy files and content to file +++++++++
+# __________________ Copy files and content to file __________________
 # BINARY FILE - get the file using requests.get()
 #             - open file using binary mode 'wb'
 #             - loop over response.object.iter_content('chunk size')
@@ -17,37 +26,48 @@
 # HTML - recommended to write in binary format as well
 # ===================================================================================================
 
-import requests
+import requests, json, pprint
 
 # getting the web page into the variable res
 # also printing the web page contents
-res = requests.get('http://www.google.com/')
+# res = requests.get('http://www.google.com/')
 
 # by calling the method below we will raise the exception if there was an error downloading the page
 # it is recommended that you use res.raise_for_status after the get request
-res.raise_for_status()
+# res.raise_for_status()
 
 # we can also use try except for the raise_for_status()
-try:
-    res.raise_for_status()
-except Exception as err:
-    print("There was a problem with a download: {}".format(err))
+# try:
+#     res.raise_for_status()
+# except Exception as err:
+#     print("There was a problem with a download: {}".format(err))
 
 # let's check the response status code
-print(res.status_code)
+# print(res.status_code)
 
 # let's copy the html back into the file
-html_file = open('html.html', 'w')
-html_file.write(res.text)
-html_file.close()
+# html_file = open('html.html', 'w')
+# html_file.write(res.text)
+# html_file.close()
 
 # let's copy binary file onto the disk
-res = requests.get('https://writeitdown31days.files.wordpress.com/2015/12/hello-picture.gif')
-res.raise_for_status()
+# res = requests.get('https://writeitdown31days.files.wordpress.com/2015/12/hello-picture.gif')
+# res.raise_for_status()
 
-binary_file = open('binary.jpeg', 'wb')
+# binary_file = open('binary.jpeg', 'wb')
 
-for chunk in res.iter_content(1000):
-    binary_file.write(chunk)
-binary_file.close()
+# for chunk in res.iter_content(1000):
+#     binary_file.write(chunk)
+# binary_file.close()
 
+# Let's prepare the list of parameters to send to the server
+# payload = {'hello': 'world'}
+# r = requests.get('http://www.facebook.com', params=payload)
+# print(r.url)
+# print(r.status_code)
+
+r = requests.get('https://api.github.com/events')
+print(json.loads(r.text))
+
+with open('json.txt', 'w') as file_object:
+    json.dump(r.text, file_object, indent=4)
---
+++ 2_Modules/8_Selenium.py
@@ -0,0 +1,95 @@
+#! /library/Frameworks/Python.framework/Versions/3.5/python3.5
+# ===================================================================================================
+# ______________________Selenium module ______________________
+# First off you need to import selenium
+# You need to use 'from selenium import webdriver'
+# variable = webdriver.driver_class() - then you can create variable with certain browser class
+# variable.get() - will open URL with the chosen browser
+# ______________________Selenium search ______________________
+# find_element - search for 1 object that firt matches your query
+# find_elements - retunrs all objects that match your query
+# If selenium does not find anything it raises an exception 'NoSuchElement'
+# if you do not want program to crash use try except
+
+# --------Elements that use CSS class name
+# browser.find_element_by_class_name(name)
+# browser.find_elements_by_class_name(name)
+# --------Elements that match the CSS selector
+# browser.find_element_by_css_selector(selector)
+# browser.find_elements_by_css_selector(selector)
+# --------Elements with a matching id attribute value
+# browser.find_element_by_id(id)
+# browser.find_elements_by_id(id)
+# --------<a> elements that completely match the text provided
+# browser.find_element_by_link_text(text)
+# browser.find_elements_by_link_text(text)
+# --------<a> elements that contain the text provided
+# browser.find_element_by_partial_link_text(text)
+# browser.find_elements_by_partial_link_text(text)
+# --------Elements with a matching name attribute value
+# browser.find_element_by_name(name)
+# browser.find_elements_by_name(name)
+# --------Elements with a matching tag name (case insensitive; an <a> element is matched by 'a' and 'A')
+# browser.find_element_by_tag_name(name)
+# browser.find_elements_by_tag_name(name)
+# ______________________Attributes or methods for selenium object ______________________
+# tag_name              - The tag name, such as 'a' for an <a> element
+# get_attribute(name)   - The value for the element’s name attribute
+# text                  - The text within the element, such as 'hello' in <span>hello</span>
+# clear()               - For text field or text area elements, clears the text typed into it
+# is_displayed()        - Returns True if the element is visible; otherwise returns False
+# is_enabled()          - For input elements, returns True if the element is enabled; otherwise returns False
+# is_selected()         - For checkbox or radio button elements, returns True if the element is selected; otherwise returns False
+# location              - A dictionary with keys 'x' and 'y' for the position of the element in the page
+# click() - click on the link
+# send_key() - sending keystrokes to the forms on the webpage
+# submit() - submit or click the button
+# ______________________Module for sending special keys ______________________
+# You need to import it first
+# from selenium.webdriver.common.keys import Keys
+# Keys.DOWN, Keys.UP, Keys.LEFT, Keys.RIGHT - The keyboard arrow keys
+# Keys.ENTER, Keys.RETURN - The ENTER and RETURN keys
+# Keys.HOME, Keys.END, Keys.PAGE_DOWN, Keys.PAGE_UP - The home, end, pagedown, and pageup keys
+# Keys.ESCAPE, Keys.BACK_SPACE, Keys.DELETE - The ESC, BACKSPACE, and DELETE keys
+# Keys.F1, Keys.F2,..., Keys.F12 - The F1 to F12 keys at the top of the keyboard
+# Keys.TAB - The TAB key
+# ______________________Clicking brower buttons ______________________
+# browser.back()
+# browser.forward()
+# browser.refresh()
+# browser.quit()
+# ===================================================================================================
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+browser = webdriver.Firefox()
+browser.get('http://inventwithpython.com')
+try:
+    elem = browser.find_element_by_class_name('bookcover')
+    print('Found {} element with that class name'.format(elem.tag_name))
+except:
+    print('Was not able to find any elements')
+
+linkElem = browser.find_element_by_link_text('Read It Online')
+linkElem.click()
+
+# Filling out email form
+# browser.get('https://mail.yahoo.com')
+# emailElem = browser.find_element_by_id('login-username')
+# emailElem.send_keys('not_my_real_email')
+# buttonElem = browser.find_element_by_id('login-signin')
+# buttonElem.click()
+# passwordElem = browser.find_element_by_id('login-passwd')
+# passwordElem.send_keys('password')
+# passwordElem.submit()
+
+# in this example you will send special keys to the browser
+# first you find element by tag name
+# html is very generalized
+# then you can send special chanracters to browser
+
+browser.get('http://nostarch.com')
+htmlElem = browser.find_element_by_tag_name('html')
+htmlElem.send_keys(Keys.END)     # scrolls to bottom
+htmlElem.send_keys(Keys.HOME)    # scrolls to top
