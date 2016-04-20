5ea0b09f1bc3751bdb58785ee3597d9c903cce42
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-04-20 14:41:15 -0700

    Updated

--- 3_Machine_learning/4_Perceptron/1_Perceptron.py
+++ 3_Machine_learning/4_Perceptron/1_Perceptron.py
@@ -15,14 +15,22 @@ class Perceptron():
         self.eta = eta
         self.n_iter = n_iter
 
+    def predict(self, X):
+        """ Return class label after unit step"""
+        return np.where(self.net_input(X) >= 0, 1, -1)
+
+    def net_input(self, X):
+        """ Calculate net input"""
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
     def fit(self, X, y):
-        """Processing training data
+        """Training perceptron
             X: array of training vectors, shape = [n_samples, n_features]
-            y: array of target feature values, shape = [n_samples] """
-        self.w_ = np.zeros(1 + X.shape[1])  # determining the number of weights, need +1 since have w0
+            y: vector of correct predictions, shape = [n_samples] """
+        self.w_ = np.zeros(1 + X.shape[1])  # need +1 weight since w0, all weights are 0 initially
         self.errors_ = []
 
-        for _ in range(self.n_iter):
+        for _ in range(self.n_iter):  # here we determine the iterations based on epochs
             errors = 0
             for xi, target in zip(X, y):
                 update = self.eta * (target - self.predict(xi))
@@ -32,13 +40,29 @@ class Perceptron():
             self.errors_.append(errors)
         return self
 
-    def net_input(self, X):
-        """ Calculate net input"""
-        return np.dot(X, self.w_[1:]) + self.w_[0]
+# ---------------------------------------------------------------------------------------------------
 
-    def predict(self, X):
-        """ Return class label after unit step"""
-        return np.where(self.net_input(X) >= 0, 1, -1)
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(X[y == cl, 0], X[y == cl, 1], alpha=0.8, c = cmap(idx), marker=markers[idx], label=cl)
+
 # ---------------------------------------------------------------------------------------------------
 # let's use pandas to import the dataset
 df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header = None)
@@ -59,7 +83,7 @@ plt.ylabel('sepal length [cm]')
 plt.legend(loc='upper left')
 plt.show()
 
-# now let;s train our perceptron with the data that we just extracted
+# now let's train our perceptron with the data that we just extracted
 ppn = Perceptron(eta=0.1, n_iter=100)
 ppn.fit(X, y)
 plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
@@ -67,46 +91,11 @@ plt.xlabel('Epochs')
 plt.ylabel('Number of misclassifications')
 plt.show()
 
-# let's implement the function that colors the graph for us
-
-
-def plot_decision_regions(X, y, classifier, resolution=0.02):
-    # setup marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    # plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha = 0.4, cmap = cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    # plot class samples
-    for idx, cl in enumerate( np.unique( y)):
-        plt.scatter(x = X[y == cl, 0], y = X[y == cl, 1], alpha=0.8, c = cmap(idx), marker=markers[idx], label=cl)
-
-
+# let's plot the contour plot
 plot_decision_regions(X, y, classifier=ppn)
 plt.xlabel('sepal length [cm]')
 plt.ylabel(' petal length [cm]')
 plt.legend( loc ='upper left')
 plt.show()
 
-print(ppn.w_)
-
-x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, 0.02), np.arange(x2_min, x2_max, 0.02))
-print('shape', xx1.shape)
-print(xx2)
-print(np.array([xx1.ravel(), xx2.ravel()]))
-print(X)
-Z = ppn.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-print(Z)
-
-print(np.dot(np.array([[1, 2],[3, 4]]), [1, 2]))
+print('Calculated weights are: ', ppn.w_)
