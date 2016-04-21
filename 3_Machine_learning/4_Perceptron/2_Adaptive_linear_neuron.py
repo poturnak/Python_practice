5ae2c370cdd72feb066d635cecc1ea7e7f5a1043
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-04-21 12:25:12 -0700

    Updated

--- 3_Machine_learning/4_Perceptron/2_Adaptive_linear_neuron.py
+++ 3_Machine_learning/4_Perceptron/2_Adaptive_linear_neuron.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas as pd
 
 
 class AdalineGD( object):
@@ -13,24 +14,25 @@ class AdalineGD( object):
     def __init__(self, eta = 0.01, n_iter = 50):
         self.eta = eta
         self.n_iter = n_iter
-    def fit( self, X, y):
+
+    def fit(self, X, y):
         """ Train ADALINE
             X : {array-like}, shape = [n_samples, n_features] Training vectors
             y : array-like, shape = [n_samples] Target values"""
-        self.w_ = np.zeros(1 + X.shape[ 1])
+        self.w_ = np.zeros(1 + X.shape[1])
         self.cost_ = []
         for i in range(self.n_iter):
             output = self.net_input(X)
             errors = (y - output)
             self.w_[1:] += self.eta * X.T.dot(errors)
-            self.w_[ 0] += self.eta * errors.sum()
-            cost = (errors ** 2). sum() / 2.0
-            self.cost_. append( cost)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors ** 2).sum() / 2.0
+            self.cost_.append(cost)
         return self
 
     def net_input(self, X):
         """ Calculate net input"""
-        return np.dot( X, self.w_[ 1:]) + self.w_[ 0]
+        return np.dot(X, self.w_[1:]) + self.w_[ 0]
 
     def activation( self, X):
         """ Compute linear activation"""
@@ -38,7 +40,22 @@ class AdalineGD( object):
 
     def predict( self, X):
         """ Return class label after unit step"""
-        return np.where( self.activation(X) >= 0.0, 1, -1)
+        return np.where(self.activation(X) >= 0.0, 1, -1)
 # ______________________________________________________________________________________________________
+
+# let's use pandas to import the dataset
+df = pd.read_csv('http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header = None)
+# print(df.tail())
+
+# next we need to extract first 100 labels that correspond to setosa and versicolor (only 2 types)
+y = df.iloc[0:100, 4].values
+y = np.where(y == 'Iris-setosa', -1, 1)
+
+# not let's build the X array and extract the 100 first features (sepal length and petal length) and put that into array
+X = df.iloc[0:100, [0, 2]].values
+
+# now let's train our ADALINE with the data that we just extracted
+ppn = AdalineGD(eta=0.1, n_iter=100)
+ppn.fit(X, y)
 
 
