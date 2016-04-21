65eef3e716b710019b538bb188dfbf94f6685377
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-04-21 13:11:24 -0700

    Updated

--- 3_Machine_learning/4_Perceptron/2_Adaptive_linear_neuron.py
+++ 3_Machine_learning/4_Perceptron/2_Adaptive_linear_neuron.py
@@ -5,6 +5,7 @@
 import numpy as np
 from matplotlib import pyplot as plt
 import pandas as pd
+from matplotlib.colors import ListedColormap
 
 
 class AdalineGD( object):
@@ -41,6 +42,26 @@ class AdalineGD( object):
     def predict( self, X):
         """ Return class label after unit step"""
         return np.where(self.activation(X) >= 0.0, 1, -1)
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(X[y == cl, 0], X[y == cl, 1], alpha=0.8, c=cmap(idx), marker=markers[idx], label=cl)
 # ______________________________________________________________________________________________________
 
 # let's use pandas to import the dataset
@@ -55,7 +76,39 @@ y = np.where(y == 'Iris-setosa', -1, 1)
 X = df.iloc[0:100, [0, 2]].values
 
 # now let's train our ADALINE with the data that we just extracted
-ppn = AdalineGD(eta=0.1, n_iter=100)
+ppn = AdalineGD(eta=0.0001, n_iter=100)
 ppn.fit(X, y)
+plot_decision_regions(X, y, classifier=ppn )
 
+# let's now plot the cost against the number of epochs and learning coefficient
+fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize =(8, 4))
+ada1 = AdalineGD(n_iter=10, eta=0.1).fit(X, y)
+ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker ='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-squared-error)')
+ax[0].set_title('Adaline - Learning rate {}'.format(ada1.eta))
 
+ada2 = AdalineGD(n_iter=10, eta=0.0001).fit(X, y)
+ax[1].plot(range(1, len(ada2. cost_) + 1), ada2.cost_, marker ='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-squared-error')
+ax[1].set_title('Adaline - Learning rate {}'.format(ada2.eta))
+plt.show()
+
+# let's standardize the feature values; we subtract the mean and divide by std
+X_std = np.copy(X)
+X_std[:, 0] = (X[:, 0] - X[:, 0].mean()) / X[:, 0].std()
+X_std[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+
+ada = AdalineGD(n_iter=15, eta=0.01)
+ada.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada)
+plt.title('Adaline - Gradient Descent')
+plt.xlabel('sepal length [standardized]')
+plt.ylabel('petal length [standardized]')
+plt.legend(loc='upper left')
+plt.show()
+plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Sum-squared-error')
+plt.show()
