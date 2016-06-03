c596d90459f4df6ec7e78e82a6763ffd0b8d46b7
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-06-03 13:33:09 -0700

    Change

--- 3_Machine_learning/4_ML_algorithms/13_Working_with_data.py
+++ 3_Machine_learning/4_ML_algorithms/13_Working_with_data.py
@@ -1,6 +1,7 @@
 #! /library/Frameworks/Python.framework/Versions/3.5/python3.5
 
 import pandas as pd
+import numpy as np
 from io import StringIO
 from sklearn.preprocessing import Imputer
 
@@ -35,10 +36,35 @@ imputed_data = imr.transform(df.values)
 print(imputed_data)
 
 # Now, we are going to take a look at how we are going to handle categorical data
+# There can be numerical data or categorical data
+# Categorical can be:
+#  - ordinal - values that can be further sorted or compared (T shirt sizes for example)
+#  - nominal - values that can not be sorted or compared (colors fo T shirst for example)
 
+df1 = pd.DataFrame([
+    ['green', 'M', 10.1, 'class1'],
+    ['red', 'L', 13.5, 'class2'],
+    ['blue', 'XL', 15.3, 'class1']
+])
+df1.columns = ['color', 'size', 'price', 'classlabel']
 
+# we can map ordinal features
+# we can create the mapping manually
+size_mapping = {'XL': 3, 'L': 2, 'M': 1}
+df1['size'] = df1['size'].map(size_mapping)
+print(df1)
 
+# if we want to transform the integers back to original values we can do inverse mapping using
+inv_size_mapping = {v: k for k, v in size_mapping.items()}
+print(inv_size_mapping)
 
-
-
+# We can also encode the class labels
+# a lot of algorithms require the class labels to be represented using the numbres instead of categorical values
+class_mapping = {label: idx for idx, label in enumerate(np.unique(df1['classlabel']))}
+df1['classlabel'] = df1['classlabel'].map(class_mapping)
+print(df1)
+# we can reverse the mapping using the following
+inv_class_mapping = {i: j for j, i in class_mapping.items()}
+df1['classlabel'] = df1['classlabel'].map(inv_class_mapping)
+print(df1)
 
