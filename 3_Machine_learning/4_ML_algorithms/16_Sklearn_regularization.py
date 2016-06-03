edb6f9eda1270bcaa5b2fa8ed5e2b90dc63cd751
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-06-03 15:11:24 -0700

    Change

--- 3_Machine_learning/4_ML_algorithms/16_Sklearn_regularization.py
+++ 3_Machine_learning/4_ML_algorithms/16_Sklearn_regularization.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from matplotlib import pyplot as plt
 
 df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
 df_wine.columns = ['Class label', 'Alcohol', 'Malic acid', 'Ash', 'Alcalinity of ash', 'Magnesium', 'Total phenols',
@@ -33,3 +34,29 @@ print(lr.score(X_test_std, y_test))
 
 print(lr.coef_)
 # we can see that some of the coefficients are 0, that means some of the feaures are potentially irrelevant
+
+fig = plt.figure()
+ax = plt.subplot(111)
+colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'pink', 'lightgreen',
+          'lightblue', 'gray', 'indigo', 'orange']
+weights, params = [], []
+for c in np.arange(-4, 6):
+    lr = LogisticRegression(penalty ='l1', C=10** c, random_state = 0)
+    lr.fit( X_train_std, y_train)
+    weights.append( lr.coef_[ 1])
+    params.append( 10** c)
+weights = np.array(weights)
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column],
+              label = df_wine.columns[column + 1],
+             color = color)
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5), 10** 5])
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center', bbox_to_anchor=(1.38, 1.03),ncol=1, fancybox=True)
+plt.show()
+
+
