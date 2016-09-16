517e8c4278c3cfe43c46897883874beda3f31d73
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-09-15 17:49:37 -0700

    Changes

--- 3_Machine_learning/4_ML_algorithms/27_Ensemble_learning.py
+++ 3_Machine_learning/4_ML_algorithms/27_Ensemble_learning.py
@@ -25,6 +25,9 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import roc_curve
+from sklearn.metrics import auc
+from matplotlib import pyplot as plt
 
 # Let's first take a look at how majority voting needs ot be done
 # Say we have three classifiers that predicted class labels
@@ -186,3 +189,38 @@ all_clf = [pipe1, clf2, pipe3, mv_clf]
 for clf, label in zip(all_clf, clf_labels):
     scores = cross_val_score(estimator=clf, X=X_train, y=y_train, cv=10, scoring='roc_auc')
     print("ROC AUC: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+colors = ['black', 'orange', 'blue', 'green']
+linestyles = [':', '--', '-.', '-']
+for clf, label, clr, ls \
+        in zip(all_clf,
+               clf_labels, colors, linestyles):
+
+    # assuming the label of the positive class is 1
+    y_pred = clf.fit(X_train,
+                     y_train).predict_proba(X_test)[:, 1]
+    fpr, tpr, thresholds = roc_curve(y_true=y_test,
+                                     y_score=y_pred)
+    roc_auc = auc(x=fpr, y=tpr)
+    plt.plot(fpr, tpr,
+             color=clr,
+             linestyle=ls,
+             label='%s (auc = %0.2f)' % (label, roc_auc))
+
+plt.legend(loc='lower right')
+plt.plot([0, 1], [0, 1],
+         linestyle='--',
+         color='gray',
+         linewidth=2)
+
+plt.xlim([-0.1, 1.1])
+plt.ylim([-0.1, 1.1])
+plt.grid()
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+
+# plt.tight_layout()
+# plt.savefig('./figures/roc.png', dpi=300)
+plt.show()
