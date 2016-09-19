35f05b9af3c1b19f8d19f830a69b3f8d594c575c
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-09-19 13:31:21 -0700

    Changes

--- 3_Machine_learning/4_ML_algorithms/28_Bagging.py
+++ 3_Machine_learning/4_ML_algorithms/28_Bagging.py
@@ -51,3 +51,23 @@ bag = BaggingClassifier(base_estimator=tree,
                         n_jobs=1,
                         random_state=1)
 
+from sklearn.metrics import accuracy_score
+
+tree = tree.fit(X_train, y_train)
+y_train_pred = tree.predict(X_train)
+y_test_pred = tree.predict(X_test)
+
+tree_train = accuracy_score(y_train, y_train_pred)
+tree_test = accuracy_score(y_test, y_test_pred)
+print('Decision tree train/test accuracies %.3f/%.3f'
+      % (tree_train, tree_test))
+
+bag = bag.fit(X_train, y_train)
+y_train_pred = bag.predict(X_train)
+y_test_pred = bag.predict(X_test)
+
+bag_train = accuracy_score(y_train, y_train_pred)
+bag_test = accuracy_score(y_test, y_test_pred)
+print('Bagging train/test accuracies %.3f/%.3f'
+      % (bag_train, bag_test))
+
