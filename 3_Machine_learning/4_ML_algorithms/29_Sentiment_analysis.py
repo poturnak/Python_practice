04b5524672524e4f5fa054053e5302b16207ed46
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-09-21 23:14:42 -0700

    Changes

--- 3_Machine_learning/4_ML_algorithms/29_Sentiment_analysis.py
+++ 3_Machine_learning/4_ML_algorithms/29_Sentiment_analysis.py
@@ -34,8 +34,8 @@ basepath = '/Users/nikolay.poturnak/Desktop/MACHINE LEARNING/IMDB dataset/'
 # df = df.reindex(np.random.permutation(df.index))
 # df.to_csv('basepath', index=False)
 
-# df = pd.read_csv(os.path.join(basepath, 'movie_data.csv'))
-# print(df.head(3))
+df = pd.read_csv(os.path.join(basepath, 'movie_data.csv'))
+print(df.head(3))
 
 
 # In this part we will learn the bag of words model that will let us represent text as feature vectors
@@ -73,7 +73,7 @@ print(bag)
 # unigram model is when you have one word representing a token
 # n-gram model can can have multiple words representing a token
 
-# oftentimes we analyze text and there are words occuring in both positive class and negative class documents
+# oftentimes we analyze text and there are words occurring in both positive class and negative class documents
 # we need to down-weight those types of words
 # for that purpose we will look at tf-idf (term frequency inverse document frequency)
 
@@ -108,5 +108,69 @@ def preprocessor(text):
     text = re.sub('[\W]+', ' ', text.lower()) + ' '.join(emoticons).replace('-', '')
     return text
 
-# let's confirm that the preprocessor works correctly
+# let's apply the preprocessor to our data
+df['review'] = df['review'].apply(preprocessor)
 
+from nltk.stem.porter import PorterStemmer
+
+porter = PorterStemmer()
+
+def tokenizer(text):
+    return text.split()
+
+def tokenizer_porter(text):
+    return [porter.stem(word) for word in text.split()]
+
+import nltk
+
+nltk.download('stopwords')
+
+from nltk.corpus import stopwords
+
+stop = stopwords.words('english')
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# in this section we will train logistic regression model for document classification
+# first we will divide the dataset into training and test sets
+
+X_train = df.loc[:25000, 'review'].values
+y_train = df.loc[:25000, 'sentiment'].values
+X_test = df.loc[25000:, 'review'].values
+y_test = df.loc[25000:, 'sentiment'].values
+
+# next we will use the gridsearch to find the optimal set of parameters for our logistic regression
+# we will use stratified 5 fold cross validation
+
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+tfidf = TfidfVectorizer(strip_accents=None,
+                        lowercase=False,
+                        preprocessor=None)
+
+param_grid = [{'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+              {'vect__ngram_range': [(1, 1)],
+               'vect__stop_words': [stop, None],
+               'vect__tokenizer': [tokenizer, tokenizer_porter],
+               'vect__use_idf':[False],
+               'vect__norm':[None],
+               'clf__penalty': ['l1', 'l2'],
+               'clf__C': [1.0, 10.0, 100.0]},
+              ]
+
+lr_tfidf = Pipeline([('vect', tfidf),
+                     ('clf', LogisticRegression(random_state=0))])
+
+gs_lr_tfidf = GridSearchCV(lr_tfidf, param_grid,
+                           scoring='accuracy',
+                           cv=5,
+                           verbose=1,
+                           n_jobs=-1)
+
+gs_lr_tfidf.fit(X_train, y_train)
