c6c69b084a22d836957f488a60fa653127fb82ec
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-09-24 17:01:12 -0700

    Changes

--- 0_Basics/19_Decorators.py
+++ 0_Basics/19_Decorators.py
@@ -28,3 +28,30 @@ def my_function():
 
 
 print(my_function())
+
+# ________________________________________________________________________________
+
+from time import sleep
+
+
+def sleep_decorator(function):
+
+    """
+    Limits how fast the function is
+    called.
+    """
+
+    def wrapper(*args, **kwargs):
+        sleep(2)
+        return function(*args, **kwargs)
+    return wrapper
+
+
+@sleep_decorator
+def print_number(num):
+    return num
+
+print(print_number(222))
+
+for num in range(1, 6):
+    print(print_number(num))
