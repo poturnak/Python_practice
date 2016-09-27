5eefc65289d9ef6f640501c154d955f51569c1ce
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-09-26 21:37:08 -0700

    Changes

--- 3_Machine_learning/4_ML_algorithms/30_Regression_analysis.py
+++ 3_Machine_learning/4_ML_algorithms/30_Regression_analysis.py
@@ -8,3 +8,15 @@
 # regression is different from pca in a way that pca minimizes the projection errors (perpendicular
 # projection from endpoints onto the pca component)
 # the regression is minimizing offsets - errors of prediction
+
+import pandas as pd
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/'
+                 'housing/housing.data',
+                 header=None,
+                 sep='\s+')
+
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS',
+              'NOX', 'RM', 'AGE', 'DIS', 'RAD',
+              'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+df.head()
