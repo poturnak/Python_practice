c2d455d25e7f984c8426dc925a164de636e626c6
nikolay.poturnak <nikolay.poturnak@gmail.com>
2016-09-27 22:06:27 -0700

    Changes

--- 3_Machine_learning/4_ML_algorithms/30_Regression_analysis.py
+++ 3_Machine_learning/4_ML_algorithms/30_Regression_analysis.py
@@ -198,12 +198,10 @@ ransac.fit(X, y)
 inlier_mask = ransac.inlier_mask_
 outlier_mask = np.logical_not(inlier_mask)
 
-line_X = np.arange(3, 10, 1)
+ine_X = np.arange(3, 10, 1)
 line_y_ransac = ransac.predict(line_X[:, np.newaxis])
-plt.scatter(X[inlier_mask], y[inlier_mask],
-            c='blue', marker='o', label='Inliers')
-plt.scatter(X[outlier_mask], y[outlier_mask],
-            c='lightgreen', marker='s', label='Outliers')
+plt.scatter(X[inlier_mask], y[inlier_mask], c='blue', marker='o', label='Inliers')
+plt.scatter(X[outlier_mask], y[outlier_mask], c='lightgreen', marker='s', label='Outliers')
 plt.plot(line_X, line_y_ransac, color='red')
 plt.xlabel('Average number of rooms [RM]')
 plt.ylabel('Price in $1000\'s [MEDV]')
@@ -212,3 +210,6 @@ plt.legend(loc='upper left')
 plt.tight_layout()
 # plt.savefig('./figures/ransac_fit.png', dpi=300)
 plt.show()
+
+print('Slope: %.3f' % ransac.estimator_.coef_[0])
+print('Intercept: %.3f' % ransac.estimator_.intercept_)
